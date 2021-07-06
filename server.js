83b21e77b6803c293f239bba4d7d0763abb4893b
RaviTeja Maramreddy <tejamaramreddy@gmail.com>
2021-07-06 09:51:38 +0530

    fixing typos

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/timer'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/myapp/index.html'));});
+'/dist/timer/index.html'));});
 app.listen(process.env.PORT || 8080);
